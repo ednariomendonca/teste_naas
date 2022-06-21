8622d3da99308b2e26552bb477b07718a2aec281
ednariomendonca <107865530+ednariomendonca@users.noreply.github.com>
2022-06-21 12:25:47 -0300

    Delete color_negative_positive.py

--- color_negative_positive.py
+++
@@ -1,9 +0,0 @@
-### Color in negative and positive values
-
-def color_negative_positive(values): 
-    red = 'background-color: red;'
-    orange = 'background-color: orange;'
-    green = 'background-color: green;'
-    default = ''
-    
-    return [red if x < 0 else orange if x == 0 else green if x > 0 else default for x in values]
